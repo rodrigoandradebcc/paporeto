39f3f20c41eb249cf47ee9adf2df1883ed5178a1
rodrigo <rodrigoandradebcc@gmail.com>
2014-12-02 18:26:50 -0300

    mod 3

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-	validates :title, :category, :published_at presence: true
+	validates :title, :category, :published_at, presence: true
 	belongs_to :category
 end
